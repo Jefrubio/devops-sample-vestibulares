3f1e1c66c38ed585569d172683b5c3df7593acf6
Jefferson Rubio de Oliveira <jerubio@live.com>
2018-05-07 20:44:09 -0300

    Update and rename app/views.py to  app/views.py
    Alterações conforme cod. do professor

--- app/views.py
+++ app/views.py
@@ -1,63 +1,63 @@
 """
 Definition of views.
 """
-
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.template import RequestContext
-from datetime import datetime
+de django.template import RequestContext
+de app.models import Curso
+de datetime import datetime
 
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        context_instance = RequestContext(request,
+def  home ( request ):
+    "" " Renderiza a home page. " ""
+    assert  isinstance (request, HttpRequest)
+    retornar renderizar (
+        pedido,
+        ' app / index.html ' ,
+        context_instance  = RequestContext (solicitação,
         {
-            'title':'Home Page',
-            'year':datetime.now().year,
+            ' título ' : ' Página inicial ' ,
+            ' ano ' : datetime.now (). ano,
         })
     )
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        context_instance = RequestContext(request,
+ contacto de def ( pedido ):
+    "" " Renderiza a página de contato. " ""
+    assert  isinstance (request, HttpRequest)
+    retornar renderizar (
+        pedido,
+        ' app / contact.html ' ,
+        context_instance  = RequestContext (solicitação,
         {
-            'title':'Contato',
-            'message':'Entre em contato conosco',
-            'year':datetime.now().year,
+            ' título ' : ' Contato ' ,
+            ' mensagem ' : ' Entre em contato conosco ' ,
+            ' ano ' : datetime.now (). ano,
         })
     )
 
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        context_instance = RequestContext(request,
+def  sobre ( pedido ):
+    "" " Renderiza a página sobre. " ""
+    assert  isinstance (request, HttpRequest)
+    retornar renderizar (
+        pedido,
+        ' app / about.html ' ,
+        context_instance  = RequestContext (solicitação,
         {
-            'title':'About',
-            'message':'Gerenciador de vestibulares',
-            'year':datetime.now().year,
+            ' title ' : ' About ' ,
+            ' mensagem ' : ' Gerenciador de vestibulares ' ,
+            ' ano ' : datetime.now (). ano,
         })
     )
 
-def cadastro_cursos(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/cadastro_cursos.html',
-        context_instance = RequestContext(request,
+def  cadastro_cursos ( request ):
+    assert  isinstance (request, HttpRequest)
+    retornar renderizar (
+        pedido,
+        ' app / cadastro_cursos.html ' ,
+        context_instance  = RequestContext (solicitação,
         {
-            'title':'Cadastro de cursos',
-            'cursos': ['ADS' , 'SI', 'CC'],
-            'year':datetime.now().year,
+            « título » : « Cadastro de cursos » ,
+#             'cursos': ['ADS', 'SI', 'CC'],
+            ' cursos ' : Curso.objects.all (),
+            ' ano ' : datetime.now (). ano,
         })
     )
-
